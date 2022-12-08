be6ee71f9b03e054ecf34975d387bd0de09f1a36
Zack Burnley <zackburnley@gmail.com>
2022-12-08 13:58:36 -0500

    Initial commit

---
+++ day1.py
@@ -0,0 +1,16 @@
+with open("puzzle1.txt") as f:
+    for line in f:
+        line = line.strip()
+        line = list(line)
+        floor, pos = 0, 0
+        basementFound = False
+        for i, c in enumerate(line):
+            if c == '(':
+                floor += 1
+            elif c == ')':
+                floor -= 1
+            if floor == -1 and not basementFound:
+                pos = i+1
+                basementFound = True
+    print("Part 1:", floor)
+    print("Part 2:", pos)
